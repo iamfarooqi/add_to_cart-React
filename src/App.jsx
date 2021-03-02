e1faba2570e2b29dddae02cf4b38843328d025c1
FAROOQI <attaurrahmanfarooqi9@gmail.com>
2021-03-02 12:30:45 +0500

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<>
+  
+
+    <div>
+      <form action="">
+        
+      </form>
+    </div>
+  
+  
+  </>);
+}
+
+export default App;
